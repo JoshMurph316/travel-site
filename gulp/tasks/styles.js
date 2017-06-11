dd59a67ce66d6644513887ce005efee8f3e3f90c
Joshmurph316 <Joshmurph316@gmail.com>
2017-06-11 14:08:15 -0400

    big push

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    nested = require('postcss-nested'),
+    cssvars = require('postcss-simple-vars'),
+    mixins = require('postcss-mixins'),
+    cssImport = require('postcss-import');
+
+gulp.task('styles', () => {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles'));
+});
