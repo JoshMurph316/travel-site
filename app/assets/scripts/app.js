9df352f9e8e7ccccac1891c36b33487358bc5f58
Joshmurph316 <Joshmurph316@gmail.com>
2017-06-13 18:56:52 -0400

    added feature styling

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,9 @@
+import $ from 'jquery';
 import MobileMenu from './modules/mobile-menu';
+import RevealOnScroll from './modules/reveal-on-scroll';
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '65%');
+
 
