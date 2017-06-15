a24815aa3ea52867eb62ca6fe8b143e9a8f70b52
Joshmurph316 <Joshmurph316@gmail.com>
2017-06-15 05:53:32 -0400

    finished header featuers

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import MobileMenu from './modules/mobile-menu';
+import StickyHeader from './modules/sticky-header';
 import RevealOnScroll from './modules/reveal-on-scroll';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '65%');
+var stickyHeader = new StickyHeader;
 
 
